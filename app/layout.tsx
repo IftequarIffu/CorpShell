60ac565d9a4e8dc7332728a930dc158648365a24
Shaik Iftequar Ahmed <iftequar.154@gmail.com>
2024-04-29 22:48:13 +0530

    Fixed the Hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,16 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
+    <html lang="en">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >
-        ...
-      </body>
+      >{children}</body>
     </html>
   );
 }
